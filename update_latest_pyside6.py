09498e70d6dead43cc4c96a4e6d0318c7c82aa81
Philippe Fremy <phil.fremy@free.fr>
2022-11-27 23:03:47 +0100

    'WIP'

---
+++ update_latest_pyside6.py
@@ -0,0 +1,118 @@
+"""Generate the upstream stubs."""
+import re
+import shutil
+import subprocess
+import sys
+import tempfile
+import zipfile
+import os
+from pathlib import Path
+from typing import List, Set, Tuple
+
+# from libcst import MetadataWrapper, parse_module
+
+from version import PYSIDE6_VERSION, PYSIDE6_VERSION_STR
+
+BASE_DIR = Path(__file__).parent
+PATH_STUBS_PYSIDE6 = BASE_DIR / 'PySide6-stubs'
+PATH_STUBS_SHIBOKEN6 = BASE_DIR / 'shiboken6-stubs'
+
+PLATFORM_WINDOWS = "win_amd64"
+PLATFORM_LINUX   = "manylinux_2_28_x86_64"
+PLATFORM_MACOSX  = "macosx_10_9_universal2"
+
+BRANCH_WINDOWS = 'upstream-pyside6-windows'
+BRANCH_LINUX   = 'upstream-pyside6-linux'
+BRANCH_MACOSX  = 'upstream-pyside6-macosx'
+
+PYSIDE6_ESSENTIALS = "PySide6_Essentials"
+PYSIDE6_ADDONS     = "PySide6_Addons"
+PYSIDE6_SHIBOKEN   = "shiboken6"
+
+
+def download_stubs(download_folder: Path, platform: str, branch_name: str) -> None:
+    """Download the stubs and copy them to pyside6-stubs folder."""
+    print(f'Git checkout branch {branch_name}')
+    subprocess.check_call(['git', 'checkout', branch_name])
+
+    download_folder = download_folder.absolute()
+
+    download_cmd = [
+        sys.executable,
+        "-m",
+        "pip",
+        "download",
+        "-d",
+        str(download_folder),
+    ]
+    if platform:
+        download_cmd += [
+            "--platform",
+            platform,
+            '--no-deps',
+        ]
+    for stub_name, target_folder in [
+        (PYSIDE6_ESSENTIALS, PATH_STUBS_PYSIDE6),
+        (PYSIDE6_ADDONS, PATH_STUBS_PYSIDE6),
+        (PYSIDE6_SHIBOKEN, PATH_STUBS_SHIBOKEN6),
+    ]:
+        print('Downloading wheel')
+        output = subprocess.run(download_cmd + [
+                f"{stub_name}=={'.'.join((str(nbr) for nbr in PYSIDE6_VERSION))}",
+            ],
+            env={
+                #                'https_proxy' : 'http://xxx.xxx.xxx.xxx:80/',
+                **os.environ
+            },
+            capture_output=True,
+            text=True,
+        ).stdout
+
+        mo = re.search(r'^\s*(Saved|File was already downloaded) (.*)\s*$', output, flags=re.MULTILINE)
+        if not mo:
+            print(f'Could not find our re in the following output\n<<<\n{output}\n>>>')
+            sys.exit(-1)
+
+        wheel_fname = mo.group(2)
+        assert stub_name.lower() in wheel_fname.lower()
+        assert platform.lower() in wheel_fname.lower()
+
+        # Extract the upstream pyi files
+        with tempfile.TemporaryDirectory() as temp_folder_str:
+            temp_folder = Path(temp_folder_str)
+            print(f"Created temporary directory {temp_folder}")
+            print(f"Extracting file {wheel_fname}")
+            with zipfile.ZipFile(wheel_fname, "r") as zip_ref:
+                zip_ref.extractall(temp_folder)
+
+            target_folder.mkdir(exist_ok=True)
+
+            # Take every pyi file from all folders and move it to "pyside6-stubs"
+            for folder in temp_folder.glob("*"):
+                print(f"Scanning folder for pyi files {folder}")
+                for extracted_file in folder.glob("*.pyi"):
+                    print(f"Copying {extracted_file.name} to {target_folder}")
+                    copy_file = target_folder / extracted_file.name
+                    shutil.copyfile(extracted_file, copy_file)
+
+        # commit new files to branch name
+        os.chdir(target_folder)
+        print(f'Git add new files')
+        subprocess.call(['git', 'add', target_folder])
+
+    print(f'Git commit new files')
+    subprocess.call(['git', 'commit', '-m', f'"upgrading upstream to version {PYSIDE6_VERSION_STR}"'])
+    print(f'Commit of new version to {target_folder} done')
+
+
+
+if __name__ == "__main__":
+    # Create pyside6-stubs folder if necessary
+    BASE_DIR.mkdir(exist_ok=True)
+
+    incoming = Path('incoming')
+    incoming.mkdir(exist_ok=True)
+
+    # Download required packages
+    # download_stubs(incoming, PLATFORM_LINUX)
+    download_stubs(incoming, PLATFORM_WINDOWS)
