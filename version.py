0e5227c3d480d7c9f19899d0822a1151ed583e79
Philippe F <phil.fremy@free.fr>
2024-03-24 10:46:03 +0100

    Target Pyside6 6.6.2

--- version.py
+++ version.py
@@ -1,5 +1,5 @@
 """Version numbers."""
-PYSIDE6_VERSION=(6, 5, 3, 0)
+PYSIDE6_VERSION=(6, 6, 2, 0)
 PYSIDE6_VERSION_STR='.'.join(str(v) for v in PYSIDE6_VERSION)
 STUB_VERSION = 0
 PYSIDE6_STUB_VERSION = '%d.%d.%d.%d.%d' % (PYSIDE6_VERSION + (STUB_VERSION,))
