07502ad3a48a069e0556d8370be75283471e0e57
Philippe F <phil.fremy@free.fr>
2025-02-14 18:45:07 +0100

    Fix incorrect QFileDialog test

--- tests/qfiledialog.py
+++ tests/qfiledialog.py
@@ -3,21 +3,42 @@ from PySide6.QtCore import QUrl
 
 
 def call_me() -> None:
-    fname, filter = '', ''
+    fname, filter = '',  ''
     fnames = ['', '']
 
-    fname, filter = QFileDialog.getOpenFileName(None, caption='caption', dir='dir', filter='filter', selectedFilter='abc')
-    fnames, filter = QFileDialog.getOpenFileNames(None, caption='caption', dir='dir', filter='filter', selectedFilter='abc')
-    fname, filter = QFileDialog.getSaveFileName(None, caption='caption', dir='dir', filter='filter', selectedFilter='abc')
+    url = QUrl()
+    urls = [url]
 
-    fname, filter = QFileDialog.getOpenFileUrl(None, caption='caption', dir=QUrl(), filter='filter',
-                                                selectedFilter='abc', supportedSchemes=['file', 'http'])
-    fnames, filter = QFileDialog.getOpenFileUrls(None, caption='caption', dir=QUrl(), filter='filter',
-                                               selectedFilter='abc', supportedSchemes=['file', 'http'])
-    fname, filter = QFileDialog.getSaveFileUrl(None, caption='caption', dir=QUrl(), filter='filter',
-                                               selectedFilter='abc', supportedSchemes=['file', 'http'])
+    fname, filter = QFileDialog.getOpenFileName(None, caption='getOPenFileName')
+    assert type(fname) == str
+    assert type(filter) == str
+
+    fnames, filter = QFileDialog.getOpenFileNames(None, caption='getOpenFileNames')
+    assert type(fnames) == list
+    assert len(fnames) >= 1
+    assert type(fnames[0]) == str
+    assert type(filter) == str
+
+    fname, filter = QFileDialog.getSaveFileName(None, caption='getSaveFileName')
+    assert type(fname) == str
+    assert type(filter) == str
+
+    url, filter = QFileDialog.getOpenFileUrl(None, caption='getOpenFileUrl')
+    assert type(url) == QUrl
+    assert type(filter) == str
+
+    urls, filter = QFileDialog.getOpenFileUrls(None, caption='getOpenFileUrls')
+    assert type(urls) == list
+    assert len(urls) >= 1
+    assert type(urls[0]) == QUrl
+    assert type(filter) == str
+
+    url, filter = QFileDialog.getSaveFileUrl(None, caption='getSaveFileUrl')
+    assert type(url) == QUrl
+    assert type(filter) == str
+
 
 
 
 # Enable me if you are ready to click 5 times on the dialog
-# call_me()
+call_me()
