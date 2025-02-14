f6c8bd428ac65c4754ea692d7d9d10f35791f281
Philippe F <phil.fremy@free.fr>
2025-02-14 19:45:33 +0100

    Keep QFileDialog non interactive

--- tests/qfiledialog.py
+++ tests/qfiledialog.py
@@ -10,30 +10,36 @@ def call_me() -> None:
     urls = [url]
 
     fname, filter = QFileDialog.getOpenFileName(None, caption='getOPenFileName')
+    print(fname, filter)
     assert type(fname) == str
     assert type(filter) == str
 
     fnames, filter = QFileDialog.getOpenFileNames(None, caption='getOpenFileNames')
+    print(fnames, filter)
     assert type(fnames) == list
-    assert len(fnames) >= 1
-    assert type(fnames[0]) == str
+    if fnames:
+        assert type(fnames[0]) == str
     assert type(filter) == str
 
     fname, filter = QFileDialog.getSaveFileName(None, caption='getSaveFileName')
+    print(fname, filter)
     assert type(fname) == str
     assert type(filter) == str
 
     url, filter = QFileDialog.getOpenFileUrl(None, caption='getOpenFileUrl')
+    print(url, filter)
     assert type(url) == QUrl
     assert type(filter) == str
 
     urls, filter = QFileDialog.getOpenFileUrls(None, caption='getOpenFileUrls')
+    print(urls, filter)
     assert type(urls) == list
-    assert len(urls) >= 1
-    assert type(urls[0]) == QUrl
+    if urls:
+        assert type(urls[0]) == QUrl
     assert type(filter) == str
 
     url, filter = QFileDialog.getSaveFileUrl(None, caption='getSaveFileUrl')
+    print(url, filter)
     assert type(url) == QUrl
     assert type(filter) == str
 
@@ -41,4 +47,4 @@ def call_me() -> None:
 
 
 # Enable me if you are ready to click 5 times on the dialog
-call_me()
+# call_me()
