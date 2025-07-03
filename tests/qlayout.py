408f586ea1492cd97239fb9041c17041468e9278
James Horsley <jbhorsley@gmail.com>
2025-07-03 17:15:55 +0100

    Add misc tests

--- tests/qlayout.py
+++ tests/qlayout.py
@@ -1,5 +1,19 @@
-from PySide6.QtWidgets import QGridLayout, QHBoxLayout, QVBoxLayout, QLayout
+from typing_extensions import override
+from PySide6.QtWidgets import QGridLayout, QHBoxLayout, QVBoxLayout, QLayout, QLayoutItem
 
 a = QGridLayout(parent=None)
 b = QHBoxLayout(parent=None)
 c = QVBoxLayout(parent=None)
+
+class TestLayout(QLayout):
+    # These can return None
+    @override
+    def itemAt(self, index: int) -> 'QLayoutItem | None':
+        return super().itemAt(index)
+    
+    @override
+    def takeAt(self, index: int) -> 'QLayoutItem | None':
+        return super().takeAt(index)
+
+assert a.itemAt(0) is None
+assert a.takeAt(0) is None
--- tests/qpixmap.py
+++ tests/qpixmap.py
@@ -1,7 +1,12 @@
-from PySide6.QtCore import Qt
+from PySide6.QtCore import QByteArray, Qt
 from PySide6.QtGui import QPixmap
 
 emptyPixmap = QPixmap(16, 16)
 emptyPixmap.fill(Qt.GlobalColor.transparent)
 emptyPixmap.fill('white')
 emptyPixmap.fill(0xFFFFFF)
+
+# Should accept str for format
+worlds_smallest_png = QByteArray.fromBase64(b"iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQAAAAA3bvkkAAAACklEQVR4AWNgAAAAAgABc3UBGAAAAABJRU5ErkJggg==")
+load_pixmap = QPixmap()
+load_pixmap.loadFromData(worlds_smallest_png, "PNG")
